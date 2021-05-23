612a04ee5a335b54322cb7b7f8b9f85df0dade07
Orendra Singh <orendrasingh@gmail.com>
2021-05-24 02:43:42 +0530

    Added some database entries and reversed order of objects

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -7,9 +7,9 @@ def index(request):
     extraSkills=ExtraSkills.objects.all()
     keySkills=KeySkills.objects.all()
     degSkills=DegSkills.objects.all()
-    experience=Experience.objects.all()
-    education=Education.objects.all()
-    recntWork=recentWork.objects.all()
-    pProjects=personalProjects.objects.all()
+    experience=Experience.objects.all().order_by('-id')
+    education=Education.objects.all().order_by('-id')
+    recntWork=recentWork.objects.all().order_by('-id')
+    pProjects=personalProjects.objects.all().order_by('-id')
     abo=about.objects.all()[0]
     return render(request,'index.html',{"popup":popup,"extraSkills":extraSkills,"keySkills":keySkills,"degSkills":degSkills,"experience":experience,"education":education,"recentWork":recntWork,"personalProject":pProjects,"testimonial":testimoni,"about":abo})
