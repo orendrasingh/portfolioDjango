1b038ddd7a03a560f2beebdf3b5f0ea91a6cfb64
Orendra Singh <orendrasingh@gmail.com>
2021-06-03 01:50:49 +0530

    added dynamic fields:

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -4,12 +4,17 @@ from .models import *
 def index(request):
     popup=notification.objects.get(id=1)
     testimoni=testimonial.objects.get(id=1)
-    extraSkills=ExtraSkills.objects.all()
-    keySkills=KeySkills.objects.all()
-    degSkills=DegSkills.objects.all()
+    extraSkills=ExtraSkills.objects.all().order_by("priority")
+    keySkills=KeySkills.objects.all().order_by("priority")
+    degSkills=DegSkills.objects.all().order_by("priority")
     experience=Experience.objects.all().order_by('-id')
     education=Education.objects.all().order_by('-id')
     recntWork=recentWork.objects.all().order_by('-id')
     pProjects=personalProjects.objects.all().order_by('-id')
     abo=about.objects.all()[0]
-    return render(request,'index.html',{"popup":popup,"extraSkills":extraSkills,"keySkills":keySkills,"degSkills":degSkills,"experience":experience,"education":education,"recentWork":recntWork,"personalProject":pProjects,"testimonial":testimoni,"about":abo})
+    profile=userProfile.objects.all()[0]
+    endors=endorsement.objects.all()
+    certificates=certification.objects.all()
+    companies=companiesWorkedFor.objects.all()
+    sections=sectionEnableDisable.objects.all()[0]
+    return render(request,'index.html',{"popup":popup,"sections":sections,"certificates":certificates,"comapnies":companies,"endorsment":endors,"profile":profile,"extraSkills":extraSkills,"keySkills":keySkills,"degSkills":degSkills,"experience":experience,"education":education,"recentWork":recntWork,"personalProject":pProjects,"testimonial":testimoni,"about":abo})
