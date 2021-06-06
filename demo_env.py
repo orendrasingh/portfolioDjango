73386b3c12200acbc7d532ea16be45c89226fcd9
Orendra Singh <orendrasingh@gmail.com>
2021-06-07 01:32:52 +0530

    fixed contact us

--- demo_env.py
+++ demo_env.py
@@ -10,3 +10,11 @@ SESSION_COOKIE_SECURE=False
 SECURE_SSL_REDIRECT=False
 CSRF_COOKIE_SECURE=False
 SECURE_BROWSER_XSS_FILTER=False
+
+#smtp credentials if you don't want to use just remove these fields from django setting
+
+EMAIL_HOST = 'your smtp host address'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your username'
+EMAIL_HOST_PASSWORD = 'your password'
